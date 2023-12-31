166c1f490f947ae7ee3f704f420f11403372facf
Ahmedomar74 <aoamar460744@gmail.com>
2023-12-31 11:23:58 +0200

    fetch similar books

--- lib/Feature/home/data/repos/home_repo_impl.dart
+++ lib/Feature/home/data/repos/home_repo_impl.dart
@@ -57,4 +57,32 @@ class HomeRepoImpl implements HomeRepo {
       );
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+        endPiont:
+            'volumes?Filtering=free-ebooks&sorting=relevance&q=computer programming',
+      );
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/Feature/home/presentation/views/widgets/similler_books_list_view.dart
+++ lib/Feature/home/presentation/views/widgets/similler_books_list_view.dart
@@ -1,24 +1,38 @@
+import 'package:booklyapp/Feature/home/presentation/manger/similer_books_cubit/similar_books_cubit.dart';
 import 'package:booklyapp/Feature/home/presentation/views/widgets/custom_list_item.dart';
+import 'package:booklyapp/core/widgets/custom_error_widget.dart';
+import 'package:booklyapp/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListViewState extends StatelessWidget {
   const SimilarBooksListViewState({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .19,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(right: 16),
-              child: CustomBookImage(
-                imageUrl:
-                    'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg',
-              ),
-            );
-          }),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .19,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(right: 16),
+                    child: CustomBookImage(
+                      imageUrl:
+                          'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg',
+                    ),
+                  );
+                }),
+          );
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
